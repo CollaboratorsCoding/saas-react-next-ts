35bb18f6b61ed8fc4192ed7688ebb970c4d0484c
Andrew Tomash <tomasz9201@gmail.com>
2020-03-26 13:56:14 +0200

    added antd, styled-components, storybook

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   useTabs: false,
   printWidth: 80,
+  endOfLine: 'auto',
   singleQuote: true,
   trailingComma: 'es5',
   jsxBracketSameLine: true,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { DatePicker, Rate } from 'antd';
+import TestButton from '../src/components/Button';
 
 const Home: React.SFC = () => {
   return (
     <div className="mainContainer">
       <div className="container">
+        <DatePicker />
         <h1 className="heading">Welcome</h1>
+        <TestButton name="test" />
+        <Rate />
       </div>
 
       <style jsx>
